df2370b6b458c2e99f6db514b90fbc48689e9e9b
Ben Groeneveld (0912716) <0912716@hr.nl>
2024-12-10 11:11:28 +0100

    initial platformio commit with placeholder code

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include "LittleFS.h"
+
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    Serial.printf("Hello World!\n");
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+}
